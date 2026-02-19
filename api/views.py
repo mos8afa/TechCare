b9a638e2bdf23277cba324534296ac68174e94e3
Mostafa Mohamed <imos8afa@gmail.com>
2026-02-19 09:06:13 +0200

    Implement Obatin JWT with username & OTP

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 import random
 from rest_framework.response import Response
-from rest_framework.decorators import api_view , permission_classes
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view 
+from django.contrib.auth import authenticate, get_user_model
 from django.core.cache import cache
 from django.core.mail import send_mail
 from project.settings import EMAIL_HOST_USER
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['POST'])
@@ -32,14 +32,36 @@ def Login(request):
     return Response({"message": "OTP sent to your email"})
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def VerifyOTP():
-    pass
-
-
+User = get_user_model
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+def VerifyOTP(request):
+    username = request.data.get('username')
+    otp = request.data.get('otp')
+
+    user = User.objects.filter(username=username)
+
+    if not user:
+        return Response({"error": "User not found"}, status=404)
+    
+    saved_otp = cache.get(f"otp_{user.id}")
+
+    if not saved_otp:
+        return Response({"error":"OTP expired"},status=400)
+    
+    if str(saved_otp) != str(otp):
+        return Response({"error":"Invalid OTP"},status=400)
+    
+    cache.delete(f"otp_{user.id}")
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response({
+        "access":str(refresh.access_token),
+        "refresh":str(refresh)
+    })
+
+
+@api_view(['POST'])
 def Register():
     pass
