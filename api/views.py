cb78679b0223fe14997a55890b53b1d2ff781025
Mostafa Mohamed <imos8afa@gmail.com>
2026-02-19 06:40:51 +0200

    Implement API authentication login view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,45 @@
-def Login():
-    pass
+import random
+from rest_framework.response import Response
+from rest_framework.decorators import api_view , permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from django.core.cache import cache
+from django.core.mail import send_mail
+from project.settings import EMAIL_HOST_USER
 
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def Login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    otp = random.randint(10000,99999)
+
+    cache.set(f"otp_{user.id}", otp, timeout=300)
+
+    send_mail(
+        subject = "TechCare Team",
+        message = f"your verification OTP is {otp}",
+        from_email = EMAIL_HOST_USER,
+        recipient_list = [user.email]
+    )
+
+    return Response({"message": "OTP sent to your email"})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def VerifyOTP():
     pass
 
+
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def Register():
     pass
