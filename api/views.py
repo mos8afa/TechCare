c30faa8b26dc8b00d0f878e20c7d4c35047ea9cf
Mostafa Mohamed <imos8afa@gmail.com>
2026-02-24 12:47:11 +0200

    edit login api view

--- api/views.py
+++ api/views.py
@@ -21,6 +21,7 @@ def Login(request):
     otp = random.randint(100000,999999)
 
     cache.set(f"otp_{user.id}", otp, timeout=300)
+    cache.set(f"otp_attempts_{user.id}", 0, timeout=300)
 
     send_mail(
         subject = "TechCare Team",
@@ -39,20 +40,41 @@ def VerifyOTP(request):
     username = request.data.get('username')
     otp = request.data.get('otp')
 
-    user = User.objects.filter(username=username)
+    user = User.objects.filter(username=username).first()
+
+    list_otp = [
+                request.data.get('otp1'),
+                request.data.get('otp2'),
+                request.data.get('otp3'),
+                request.data.get('otp4'),
+                request.data.get('otp5'),
+                request.data.get('otp6')
+            ]
+    for otp in list_otp:
+        if not otp or not otp.isdigit():
+            return Response({"error": "Invalid OTP format"}, status=400 )
+
+    otp_str = "".join(list_otp)
+    otp_input = int(otp_str)
 
     if not user:
         return Response({"error": "User not found"}, status=404)
+    
+    attempts = cache.get(f"otp_attempts_{user.id}")
+    if attempts is not None and attempts >= 5:
+        return Response({"error": "Too many OTP attempts. Please try again later."}, status=429)
     
     saved_otp = cache.get(f"otp_{user.id}")
 
     if not saved_otp:
         return Response({"error":"OTP expired"},status=400)
     
-    if str(saved_otp) != str(otp):
+    if str(saved_otp) != str(otp_input):
+        cache.incr(f"otp_attempts_{user.id}")
         return Response({"error":"Invalid OTP"},status=400)
     
     cache.delete(f"otp_{user.id}")
+    cache.delete(f"otp_attempts_{user.id}")
 
     refresh = RefreshToken.for_user(user)
 
