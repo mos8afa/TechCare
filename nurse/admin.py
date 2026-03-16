fa8f5b827f5c5ed43307da5259fe1177654b4f02
Mostafa Mohamed <imos8afa@gmail.com>
2026-03-16 14:03:01 +0200

    solve nurse errors

--- nurse/admin.py
+++ nurse/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Service, NurseRequest
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "nurse")
+    search_fields = ("name",)
+    list_filter = ("nurse",)
+
+
+@admin.register(NurseRequest)
+class NurseRequestAdmin(admin.ModelAdmin):
+    list_display = ("id", "patient", "nurse", "date", "time", "status", "net_income")
+    list_filter = ("status", "governrate")
+    search_fields = ("patient__user__username", "nurse__user__username")
